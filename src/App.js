5759064cd439288bc981074777519e41b71c0102
Miljan <miljanrubezic@gmail.com>
2022-04-18 14:24:44 +0200

    changed readme

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import './index.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { LanesProvider } from './context/LanesContext'
 import BoardsPage from './pages/BoardsPage'
-import { DragDropContext } from 'react-dnd-beautiful'
 import { DragDropContainer } from './context/DragDropContext'
 
 export const Content = () => (
